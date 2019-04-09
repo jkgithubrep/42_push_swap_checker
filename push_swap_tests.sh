0320bd3419bd6a6724823dd2b210f7a4b58a9c53
Jalil KETTANI <jkettani@e2r2p3.42.fr>
2019-04-09 19:38:43 +0200

    Redirect stderr

--- push_swap_tests.sh
+++ push_swap_tests.sh
@@ -7,19 +7,23 @@ random_nbr_list ()
 
 launch_tests() {
 	[ -f $TEMP_FILE ] && rm -f $TEMP_FILE
-	local index=0;
-	local sum=0;
+	local index=0
+	local sum=0
+	local max=0
+	local min=10000000
 	while [ "$index" -lt "$1" ];
 	do
-		printf "> Test %03d:" $index | tee -a $TEMP_FILE
+		printf "> Test %03d:" $((index + 1)) | tee -a $TEMP_FILE
 		local nums=`random_nbr_list "$2" "$3" "$4"`
 		printf " %s\n" "$nums" >> $TEMP_FILE
-		res=`./push_swap $nums | ./checker $nums | cat -e`
+		res=`./push_swap $nums 2> /dev/null | ./checker $nums 2> /dev/null | cat -e`
 		if [ "$res" = "OK$" ]; then
 			printf " ✅ "
-			local nb_instr=`./push_swap $nums | wc -l | bc`
+			local nb_instr=`./push_swap $nums 2> /dev/null | wc -l | bc`
 			sum=$((sum + nb_instr))
 			printf " - $nb_instr"
+			[ "$nb_instr" -lt "$min" ] && min="$nb_instr"
+			[ "$nb_instr" -gt "$max" ] && max="$nb_instr"
 		else
 			printf " ❌ "
 		fi
@@ -27,8 +31,10 @@ launch_tests() {
 		((index++))
 	done
 	local average=`echo "$sum/$index" | bc`
-	printf "Average = %d\n" $average
-	rm -f $TEMP_FILE
+	printf "\n%s\n" "SUMMARY:"
+	printf "  • Average = %d\n" "$average"
+	printf "  • Min = %d\n" "$min"
+	printf "  • Max = %d\n" "$max"
 }
 
 ARGS="$@"
